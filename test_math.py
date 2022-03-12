501f0a29991b31496624687e7d4f61740f29527a
Kabboud <98329660+Kabboud@users.noreply.github.com>
2022-03-12 13:59:12 -0500

    Create test_math.py

---
+++ test_math.py
@@ -0,0 +1,12 @@
+import unittest
+from functions import cps5415
+
+class TestSampleMethods(unittest.TestCase):
+      def test_cps5415(self):
+        """
+        Test cps5415 function
+        """
+        self.assertEqual(cps5415(7), 42)
+        
+if __name__ == '__main__':
+    unittest.main()
